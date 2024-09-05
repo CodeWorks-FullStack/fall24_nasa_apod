a423470518c21feaa29268d36af58e520d124d78
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-05 09:52:54 -0600

    added property to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { Picture } from './models/Picture.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -6,6 +7,9 @@ class ObservableAppState extends EventEmitter {
   user = null
   /**@type {import('./models/Account.js').Account | null} */
   account = null
+
+  /**@type {Picture} */
+  picture = null
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
