bd6fbe24671b0dc134ef6b46f7f5964614a12407
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-05 14:55:14 -0600

    lil better styling

--- app/models/Picture.js
+++ app/models/Picture.js
@@ -31,10 +31,10 @@ export class Picture {
 
   get listHTMLTemplate() {
     return /*html*/`
-    <div class="mb-3 position-relative p-2">
-      <img  src="${this.imgUrl}" alt="${this.author} took this picture on ${this.date}" class="my-picture rounded">
-      <i onclick="app.SandboxPicturesController.deletePicture('${this.id}')" class="mdi mdi-close-thick text-danger position-absolute top-0 fs-2" title="Delete Picture" role="button"></i>
-      <p class="position-absolute bottom-0 fs-2 selectable" title="Display this picture"  onclick="app.NasaPicturesController.getNasaPictureByDate('${this.date}')" role="button">${this.date}</p>
+    <div class="mb-3 position-relative">
+      <img src="${this.imgUrl}" alt="${this.author} took this picture on ${this.date}" class="my-picture rounded">
+      <i onclick="app.SandboxPicturesController.deletePicture('${this.id}')" class="mdi mdi-close-thick text-danger delete-icon" title="Delete Picture" role="button"></i>
+      <p class="fs-2 selectable my-picture-date" title="Display this picture"  onclick="app.NasaPicturesController.getNasaPictureByDate('${this.date}')" role="button">${this.date}</p>
     </div>
     `
   }
