0be8212e5280abf814889b4032d54193246ea5d3
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-05 11:52:34 -0600

    getting my pictures

--- app/controllers/SandboxPicturesController.js
+++ app/controllers/SandboxPicturesController.js
@@ -1,15 +1,27 @@
+import { AppState } from "../AppState.js";
 import { sandboxPicturesService } from "../services/SandboxPicturesService.js";
 import { Pop } from "../utils/Pop.js";
 
 export class SandboxPicturesController {
   constructor() {
     console.log('🥪📦📷🎮');
+    // NOTE get my pictures after I log in
+    AppState.on('user', this.getMyPictures)
   }
 
   async savePicture() {
     try {
       await sandboxPicturesService.savePicture()
       Pop.success('Picture was saved!')
+    } catch (error) {
+      Pop.error(error)
+      console.error(error);
+    }
+  }
+
+  async getMyPictures() {
+    try {
+      await sandboxPicturesService.getMyPictures()
     } catch (error) {
       Pop.error(error)
       console.error(error);
--- app/services/SandboxPicturesService.js
+++ app/services/SandboxPicturesService.js
@@ -2,6 +2,10 @@ import { AppState } from "../AppState.js";
 import { api } from "./AxiosService.js"
 
 class SandboxPicturesService {
+  async getMyPictures() {
+    const response = await api.get('api/apods')
+    console.log('GOT MY PICTURES 📷🌌🛰️', response.data);
+  }
   async savePicture() {
     const response = await api.post('api/apods', AppState.picture)
     console.log('SAVED PICTURE 💾🌌📷', response.data);
