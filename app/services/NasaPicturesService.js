59eacfbdcf838211d5f5e8e3a9f878067bc57c79
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-05 09:56:13 -0600

    storing picture in appstate

--- app/services/NasaPicturesService.js
+++ app/services/NasaPicturesService.js
@@ -1,9 +1,13 @@
+import { AppState } from "../AppState.js";
+import { Picture } from "../models/Picture.js";
 import { nasaAPI } from "./AxiosService.js"
 
 class NasaPicturesService {
   async getNasaPicture() {
     const response = await nasaAPI.get('planetary/apod?api_key=2DRMc8Ah0Y0rljaAOqEQtjiMY3f6ZrfswxzINUX1')
     console.log('GOT PICTURE 🌌📷', response.data);
+    const newPicture = new Picture(response.data)
+    AppState.picture = newPicture
   }
 }
 
